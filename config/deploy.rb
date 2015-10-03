e3a7efc65cfe94c3fd93d0cdbe46f13c457e067c
sjdarshan <sudarshangond@gmail.com>
2015-10-03 17:33:06 +0530

    Set up Puma, Nginx & Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,4 @@
+server '54.254.190.184', roles: [:web, :app, :db], primary: true
 # Change these
 set :repo_url,        'git@github.com:RationPaani/old.git'
 set :application,     'RationPaani'
@@ -6,11 +7,11 @@ set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
 # Don't change these unless you know what you're doing
-set :pty,             true
+et :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/ubuntu/RationPaani"
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
