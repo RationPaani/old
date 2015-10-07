4305d45874d3591efc65054767f6e874561d201a
sjdarshan <sudarshangond@gmail.com>
2015-10-07 16:46:30 +0530

    symlink again and rverified display

--- config/deploy.rb
+++ config/deploy.rb
@@ -73,15 +73,15 @@ namespace :deploy do
     end
   end
 
-before "deploy:restart", :symlink_directories
-task :symlink_directories do
-  	execute "ln -nfs #{shared_path}/public/images #{release_path}/public/images"
-end
-
   before :starting,     :check_revision
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
   after  :finishing,    :restart
+end
+
+before "deploy:restart", :symlink_directories
+task :symlink_directories do
+  	execute "ln -nfs #{shared_path}/public/images #{release_path}/public/images"
 end
 # ps aux | grep puma    # Get puma pid
 # kill -s SIGUSR2 pid   # Restart puma
